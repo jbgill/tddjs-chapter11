23e3daa1032a97c7f9ffa476007647eaddcc8ac2
JB Gill <jbgill@familysearch.org>
2013-03-07 08:59:24 -0700

    step 2 done

--- observer_util.js
+++ observer_util.js
@@ -1,1 +1,17 @@
-// placeholder observer_util
+// STEP 2:  Make the test pass by implementing the test's requirements
+// We are doing all of the pieces necessary to make the test pass here,
+// but it may be a good idea to address one test error at a time like the 
+// book does.  As the book states, at first we will "do the simplest 
+// thing that will work, no matter how dirty it feels", then review the 
+// code once the test passes.
+
+var Observable = function() {
+
+};
+
+var addObserver = function(observer) {
+  this.observers = [observer];
+};
+Observable.prototype.addObserver = addObserver;
+
+exports.Observable = Observable;
--- test/observer_util_test.js
+++ test/observer_util_test.js
@@ -1,4 +1,4 @@
-// STEP 1:  first test.  This should fail because the implementation isn't written yet
+// STEP 2:  first test.  Over in the obeserver source, we make it pass
 
 var should = require('should');
 
@@ -6,16 +6,17 @@ var observer_util = require('../observer_util');
 
 describe('Observable test suite', function() {
 
-	describe('ObservableAddObserverTest', function() {
-		it('should store the observer function');
+  describe('ObservableAddObserverTest', function() {
+    it('should store the observer function', function() {
+      var observable = new observer_util.Observable();
+      var observer = function() {};
 
-		var observable = new observer_util.Observable();
-		var observer = function() {};
+      observable.addObserver(observer);
 
-		observable.addObserver(observer);
+      // we know this is a bad condition - we'll get to it later
+      observer.should.equal(observable.observers[0]);
+    });
+  });
 
-        // we know this is a bad condition - we'll get to it later
-		observer.should.equal(observable.observers[0]);
 
-	});
 });
